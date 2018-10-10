7b6717b086e5e51fed12802e4919d96868f4ee46
Daniel Becker <danielbecker@MacBook-Pro.local>
2018-10-10 11:20:14 -0600

    bands app with working index and show routs

---
+++ model/bands.js
@@ -0,0 +1,29 @@
+const bands = [
+    {
+        name: "Umphrye's McGee",
+        members: ['Jake Cinninger', 'Brendan Bayless', 'Ryan Staysik', 'Kris Meyers', 'Joel Cummins', 'Andy Farang'],
+        homeTown: 'South Bend',
+        started: 1997
+    }, 
+    {
+        name: 'Widespread Panic',
+        members: ['John Bell', 'Jimmy Herring', 'Dave Schools', 'Duane Trucks',"John 'JoJo' Herman", "Domingo 'Sunny' Ortiz"],
+        homeTown: 'Athens',
+        started: 1986
+    },
+    {
+        name: "Glass Animals",
+        members: ['Dave Bayley', 'Drew MacFarlane', 'Edmund Irwin-Singer', 'Joe Seaward'],
+        homeTown: 'Oxford',
+        started: 2012
+    },
+    {
+        name: "St. Paul & the Broken Bones",
+        members: ['Paul Janeway', 'Browan Lollar', 'Jesse Philips', 'Andrew Lee', 'Al Gamble', 'Allen Branstetter', 'Amari Ansari', 'Chad Fisher'],
+        homeTown: 'Bermingham',
+        started: 2012
+    }
+]
+
+
+module.exports = bands;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const Bands = require('./model/bands');
+
+
+app.get('/', (req, res) => {
+    res.send('This is the bands app')
+})
+
+
+app.get('/bands', (req, res) => {
+    res.render('index.ejs', {
+        bands: Bands
+    })
+})
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Bands app is listening on port 3000')
+});
